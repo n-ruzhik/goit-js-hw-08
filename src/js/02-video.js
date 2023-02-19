5cb6181f8133359045993bddd3d379d0745c907b
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-02-19 15:00:20 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import vimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+const PLAYER_KEY_TIME = 'videoplayer-current-time';
+
+const getCurrentTime = ({ seconds }) => {
+  localStorage.setItem(PLAYER_KEY_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(PLAYER_KEY_TIME) || 0);
